7dcb2efac18c92186a2995c91527f3e0a98d48da
Test User <test@example.com>
2026-03-19 11:52:31 +0800

    修复模板渲染变量缺失问题

--- core/src/main/java/org/hyw/tools/generator/template/TemplateContextBuilder.java
+++ core/src/main/java/org/hyw/tools/generator/template/TemplateContextBuilder.java
@@ -224,23 +224,28 @@ public class TemplateContextBuilder {
         RenderContext globalContext = buildGlobalContext();
         RenderContext tableContext = globalContext.createChildContext();
         
-        // 设置当前表
+        // 设置当前表 - 这允许模板通过${table.xxx}访问表的所有属性
         tableContext.table(table);
         
-        // 添加表特定的变量
+        // 添加表特定的变量 - 这些允许模板直接通过${xxx}访问
         tableContext
             .put("entityName", table.getBeanName())
             .put("entityNameLower", table.getLowercaseBeanName())
             .put("tableName", table.getName())
             .put("tableComment", table.getComment())
-            .put("fields", table.getFields())
+            .put("fields", table.getFields())  // 允许模板通过${fields}直接访问
             .put("fieldCount", table.getFieldsSize())
             .put("primaryKeyField", table.getPrimaryKeyField())
             .put("primaryKeyClass", table.getPrimaryKeyClass())
             .put("hasPrimaryKey", table.hasPrimarykeys())
             .put("isCompositePrimaryKey", table.isCompositePrimaryKey())
             .put("primaryKeyInfo", table.getPrimaryKeyInfo())
-            .put("importPackages", table.getImportPackages());
+            .put("importPackages", table.getImportPackages())
+            .put("className", table.getBeanName() + "Controller")  // 添加className变量
+            .put("mapperName", table.getBeanName() + "Mapper")     // 添加mapperName变量
+            .put("serviceName", table.getBeanName() + "Service")    // 添加serviceName变量
+            .put("serviceImplName", table.getBeanName() + "ServiceImpl") // 添加serviceImplName变量
+            .put("controllerName", table.getBeanName() + "Controller"); // 添加controllerName变量
         
         // 添加字段相关的快捷变量
         addFieldShortcuts(tableContext, table);
